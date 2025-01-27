c22ace35c6306cc3b7429bdb313c188d73cb71b7
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2025-01-27 19:48:21 -0400

    Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
+main() {
   runApp( const PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class _PerguntaAppState extends State<_PerguntaApp> {
   const PerguntaApp({super.key});
+
+  var _perguntaSelecionada = 0;
+
+  void _responder (){
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
 
 
   @override
   Widget build(BuildContext context) {
+    final perguntas=[
+      'Qual sua cor favorita?',
+      'Qual seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,7 +30,8 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: const Column(
           children: <Widget>[
-            Text('Linha 1')
+            //Text('Linha 1')
+            ElevatedButton(onPressed: onPressed: _responder, child: child,)
           ],
         ),
       ),
