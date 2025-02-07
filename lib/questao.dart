977fab4a2779695da030bea831453b309a509605
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2025-02-07 11:44:59 -0400

    Curso de Flutter.

--- lib/questao.dart
+++ lib/questao.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class Questao extends StatelessWidget {
   final String texto;
 
-  const Questao (this.texto, {super.key});
+  const Questao(this.texto);
 
   @override
   Widget build(BuildContext context) {
-    return Conteiner(
+    return Container(
       width: double.infinity,
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       child: Text(
         texto,
-        style: TextStyle(fontSize: 28),
+        style: const TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
     );
